ccbfb117e7f325295ed5f24caede0b2bce17d02e
kimyongkuk <k0603156@gmail.com>
2019-01-22 18:23:28 +0900

    Revert "20190108 notice crud react와 연결 create까지만 확인"
    This reverts commit db6e7a6c853404e773671e8cbb11af79964d35e1.

--- erp_server/settings.py
+++ erp_server/settings.py
@@ -38,9 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'notice',
-    
     'rest_framework',
     'rest_framework_swagger',
     'rest_framework.authtoken',
--- notice/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Notice(models.Model):
-    text = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
-
-    def __str__(self):
-        return self.text
